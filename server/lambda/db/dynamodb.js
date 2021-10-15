d0a5803f2f8aadf31422132221ca13b7225462eb
prabhukiran9999 <70853875+prabhukiran9999@users.noreply.github.com>
2021-10-15 10:59:26 -0400

    Update dynamodb.js

--- server/lambda/db/dynamodb.js
+++ server/lambda/db/dynamodb.js
@@ -22,7 +22,7 @@ class DynamoDBClient {
    */
   static config() {
     return {      
-      dbName: process.env.DB_NAME || 'ssp-greetings'      
+      dbName: process.env.DB_NAME || 'ssp-greetings-vm'      
     };
   }
 
