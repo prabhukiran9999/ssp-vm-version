5bc094826a4ff508f71d089517dae13510f13bde
prabhukiran9999 <70853875+prabhukiran9999@users.noreply.github.com>
2021-10-19 13:53:46 -0400

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+cd /home/ssm-user/
 sudo yum update -y
+sudo yum install -y libselinux-python
 sudo amazon-linux-extras install ansible2 -y
 sudo yum install git -y
-cd /home/ssm-user/
-git https://github.com/prabhukiran9999/ssp-vm-version.git /home/ssm-user/repos/
-cd /home/ssm-user/repos/
+git clone https://github.com/prabhukiran9999/ssp-vm-version.git /home/ssm-user/repos/
+sudo setenforce 0
+cd /home/ssm-user/repos/playbook
+export AWS_REGION=ca-central-1
 ansible-playbook dev.yml
-
-
