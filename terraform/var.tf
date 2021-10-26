1bcf60916992dd50b413c3c3a597a51ab16c6e3d
prabhukiran9999 <70853875+prabhukiran9999@users.noreply.github.com>
2021-10-25 23:42:15 -0400

    Update var.tf

--- terraform/var.tf
+++ terraform/var.tf
@@ -7,7 +7,7 @@
  }
 variable "GITHUB_REPOSITORY" {
   description = "Name of the git repo to clone"
-  default     = "github.com/bcgov/startup-sample-project-aws-virtual-machines/tree/dev"
+  default     = "github.com/bcgov/startup-sample-project-aws-virtual-machines/tree/main"
   type        = string
 }
 variable "target_group_name" {
